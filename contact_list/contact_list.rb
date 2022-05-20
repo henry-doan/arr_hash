fef89cc6bff69701d0bba633be1b40b48b5ec370
Henry Doan <henrydoan96@gmail.com>
2022-05-19 19:03:19 -0600

    end of contact list

--- contact_list/contact_list.rb
+++ contact_list/contact_list.rb
@@ -32,18 +32,22 @@ require 'pry'
   # loop 
   # recursion you are calling methods within its self 
 
+# Array of hashes of contacts
 @contacts = [
+  # info for each contact
   { first_name: 'bob', last_name: 'smith', email: 'bob@bob.com', phone: '123-123-1233', favorite: true },
   { first_name: 'jane', last_name: 'doe', email: 'jane@jane.com', phone: '321-245-2222', favorite: true },
   { first_name: 'mike', last_name: 'jones', email: 'mike@mike.com', phone: '123-444-4455', favorite: false },
 ]
 
+# welcome method
 def welcome 
   puts "Welcome to my contact list!"
   puts "---------------------------"
   main_menu
 end
 
+# main menu method
 def main_menu 
   puts "1) Create a contact"
   puts "2) View All Contacts"
@@ -51,22 +55,26 @@ def main_menu
   puts "What is it you want to do?"
   user_choice = gets.strip.to_i
 
+  # condition of what the user chooses for the menu
   if user_choice == 1  
     create_contact
-    main_menu
+    main_menu # go back to main menu
   elsif user_choice == 2
-    puts 'view all contacts'
-    main_menu
+    view_contacts
+    main_menu # go back to main menu
   elsif  user_choice == 3
     puts "Good Bye"
-    exit 
+    exit # exit the program
   else
+    # error handling for what they have not choose 1, 2 or 3
     puts "Error have to choose 1, 2, or 3"
     main_menu
   end
 end
 
+# method to create contact
 def create_contact
+  # outputs of grabbing the users info
   puts "Creating a Contact"
   puts "What is the contact first name?"
   first_name = gets.strip 
@@ -80,14 +88,53 @@ def create_contact
   puts "Are they Your favorite contact? (y/n)"
   fav = gets.strip
 
+  # ask in a loop y or n until they do
   while true 
     break if fav == 'y' || fav == 'n'
     puts "Are they Your favorite contact? (y/n)"
     fav = gets.strip
   end
 
-  
+  # change string to a boolean
+  fav_bool = false
+  if fav == 'y'
+    fav_bool = true
+  end
 
+  # create new contact hash
+  new_contact = { 
+    first_name: first_name, 
+    last_name: last_name, 
+    email: email, 
+    phone: phone, 
+    favorite: fav_bool 
+  }
+
+  # add contact to the array
+  @contacts << new_contact
+end
+
+# method to view contacts
+def view_contacts
+  puts 'View All Contacts'
+  # iterate through the array and display in a readable format
+  @contacts.each do |contact_info|
+    puts "Name: #{contact_info[:first_name]} #{contact_info[:last_name]}"
+    puts "Email: #{contact_info[:email]}"
+    puts "Phone: #{contact_info[:phone]}"
+    puts "Favorite: #{is_fav(contact_info[:favorite])}"
+    puts 
+    puts "---------"
+  end
+end
+
+# method to convert a boolean to a string
+def is_fav(favorite)
+  if favorite == true 
+    return 'Yes'
+  else 
+    return 'No'
+  end
 end
 
 welcome
